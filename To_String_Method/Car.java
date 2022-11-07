d64919b2cfdedbc4901c86a460e7be90b6d58737
eray <ekrem.kaya@std.medipol.edu.tr>
2022-11-07 05:10:16 +0300

    Add files via upload

---
+++ To_String_Method/Car.java
@@ -0,0 +1,15 @@
+	
+	public class Car {
+
+
+	 String make = "Ford";
+	 String model = "Mustang";
+	 String color = "red";
+	 int year = 2021;
+	 
+	 public String toString() {			// override method
+	  
+		return make + "\n" + model + "\n" + color + "\n" + year;
+	  
+	 } 
+}
