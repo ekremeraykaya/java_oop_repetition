147b44feb6586722938f028e5b4c027d3670c1fe
eray <ekrem.kaya@std.medipol.edu.tr>
2022-11-11 01:57:03 +0300

    Add files via upload

---
+++ Variable_Scope/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    /* 
+
+       Variable Scopes (local, global variables)
+
+
+       local =  declared inside a method
+       visible only to that method
+
+
+       global  = declared outside a method, but within a class
+       visible to all parts of a class
+
+*/
+
+        DiceRoller diceRoller = new DiceRoller();
+
+    }
+}
